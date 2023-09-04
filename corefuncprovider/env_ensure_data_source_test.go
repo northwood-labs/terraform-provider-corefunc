425bcd0c60b57c99e8811bb91e29fa194904ca96
Ryan Parman <ryan@ryanparman.com>
2023-09-04 16:25:44 -0600

    fix: Fixed the last failing test for EnvEnsure().

--- corefuncprovider/env_ensure_data_source_test.go
+++ corefuncprovider/env_ensure_data_source_test.go
@@ -3,6 +3,7 @@ package corefuncprovider
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 	"testing"
 
@@ -23,24 +24,39 @@ func TestAccEnvEnsureDataSource(t *testing.T) {
 
 		_ = os.Setenv(tc.EnvVarName, tc.SetValue)
 
-		resource.Test(t, resource.TestCase{
-			ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-			Steps: []resource.TestStep{
-				{
-					Config: providerConfig + `
+		// We expect the error to be nil.
+		if tc.ExpectedErr == nil {
+			resource.Test(t, resource.TestCase{
+				ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+				Steps: []resource.TestStep{
+					{
+						Config: providerConfig + `
 					data "corefunc_env_ensure" "env" {
 						name = "` + tc.EnvVarName + `"
 					}
 					`,
-					Check: resource.ComposeAggregateTestCheckFunc(
-						resource.TestCheckResourceAttr(
-							"data.corefunc_env_ensure.env",
-							"value",
-							tc.SetValue,
+						Check: resource.ComposeAggregateTestCheckFunc(
+							resource.TestCheckResourceAttr("data.corefunc_env_ensure.env", "value", tc.SetValue),
 						),
-					),
+					},
 				},
-			},
-		})
+			})
+		} else {
+			// We DO expect an error.
+			// https://developer.hashicorp.com/terraform/plugin/testing/testing-patterns
+			resource.Test(t, resource.TestCase{
+				ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+				Steps: []resource.TestStep{
+					{
+						Config: providerConfig + `
+                    data "corefunc_env_ensure" "env" {
+                        name = "` + tc.EnvVarName + `"
+                    }
+                    `,
+						ExpectError: regexp.MustCompile("environment variable (\\w+) is not defined"),
+					},
+				},
+			})
+		}
 	}
 }
