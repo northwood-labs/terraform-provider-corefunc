ad79c07e4bdf5381e4f01cd3bb076957bac16693
Ryan Parman <ryan@ryanparman.com>
2023-09-04 16:36:02 -0600

    docs: Update the formatting of the provider examples.

--- examples/data-sources/corefunc_str_truncate_label/data-source.tf
+++ examples/data-sources/corefunc_str_truncate_label/data-source.tf
@@ -1,5 +1,4 @@
 # Pass the prefix and the label, then limit to 64 characters
-
 data "corefunc_str_truncate_label" "label" {
   prefix     = "NW-ZZZ-CLOUD-TEST-APP-CLOUD-PROD-CRIT"
   label      = "K8S Pods Not Running Deployment Check"
@@ -7,9 +6,8 @@ data "corefunc_str_truncate_label" "label" {
 }
 
 # `value` is the read-only attribute containing the truncated label
-
 output "value" {
   value = data.corefunc_str_truncate_label.label.value
 }
 
-# => NW-ZZZ-CLOUD-TEST-APP-CLOUD-PR…: K8S Pods Not Running Deploymen…
+#=> NW-ZZZ-CLOUD-TEST-APP-CLOUD-PR…: K8S Pods Not Running Deploymen…
