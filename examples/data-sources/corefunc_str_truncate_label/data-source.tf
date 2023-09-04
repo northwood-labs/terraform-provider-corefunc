b69b67bff73b68fc4b389896ae3d48a712ab5612
Ryan Parman <ryan@ryanparman.com>
2023-09-04 15:25:55 -0600

    feat: Added the Terraform wrapper and docs for EnvEnsure().

--- examples/data-sources/corefunc_str_truncate_label/data-source.tf
+++ examples/data-sources/corefunc_str_truncate_label/data-source.tf
@@ -1,13 +1,15 @@
-# Pass the prefix and the label, then limit to 64 characters.
+# Pass the prefix and the label, then limit to 64 characters
+
 data "corefunc_str_truncate_label" "label" {
   prefix     = "NW-ZZZ-CLOUD-TEST-APP-CLOUD-PROD-CRIT"
   label      = "K8S Pods Not Running Deployment Check"
   max_length = 64
 }
 
-# `value` is the read-only attribute containing the truncated label.
+# `value` is the read-only attribute containing the truncated label
+
 output "value" {
   value = data.corefunc_str_truncate_label.label.value
 }
 
-#=> NW-ZZZ-CLOUD-TEST-APP-CLOUD-PR…: K8S Pods Not Running Deploymen…
+# => NW-ZZZ-CLOUD-TEST-APP-CLOUD-PR…: K8S Pods Not Running Deploymen…
