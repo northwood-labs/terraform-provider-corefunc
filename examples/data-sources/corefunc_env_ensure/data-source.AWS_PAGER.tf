cdd89cf769487d4bb801f4c093af446cb1f0c588
Ryan Parman <ryan@ryanparman.com>
2023-09-05 16:02:27 -0600

    docs(provider): Split the examples for env_ensure into separate code blocks.

---
+++ examples/data-sources/corefunc_env_ensure/data-source.AWS_PAGER.tf
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------
+# AWS_PAGER=""
+
+data "corefunc_env_ensure" "aws_pager" {
+  name = "AWS_PAGER"
+}
+
+output "aws_pager_value" {
+  value = data.corefunc_env_ensure.aws_pager.value
+}
+
+#=> [Error] Problem with Environment Variable: environment variable
+#=>         AWS_PAGER is not defined
