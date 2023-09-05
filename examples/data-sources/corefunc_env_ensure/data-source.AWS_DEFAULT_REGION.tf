a5b1aab169ce6338525810178bc4670bd14a06ff
Ryan Parman <ryan@ryanparman.com>
2023-09-05 16:02:27 -0600

    docs(provider): Split the examples for env_ensure into separate code blocks.

---
+++ examples/data-sources/corefunc_env_ensure/data-source.AWS_DEFAULT_REGION.tf
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------
+# AWS_DEFAULT_REGION="us-east-1"
+
+data "corefunc_env_ensure" "aws_default_region" {
+  name = "AWS_DEFAULT_REGION"
+}
+
+# `aws_default_region_value` is the read-only attribute containing the
+# value of the environment variable
+output "aws_default_region_value" {
+  value = data.corefunc_env_ensure.aws_default_region.value
+}
+
+#=> us-east-1
---
+++ examples/data-sources/corefunc_env_ensure/data-source.AWS_VAULT.tf
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------
+# AWS_VAULT="dev"
+
+data "corefunc_env_ensure" "aws_vault" {
+  name    = "AWS_VAULT"
+  pattern = "(non)?prod$" # Must end with "prod" or "nonprod".
+}
+
+output "aws_vault_value" {
+  value = data.corefunc_env_ensure.aws_vault.value
+}
+
+#=> [Error] Problem with Environment Variable: environment variable
+#=>         AWS_VAULT does not match pattern (non)?prod$
