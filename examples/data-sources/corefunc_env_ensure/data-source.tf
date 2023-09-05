1f0a9adfec17012b6b19ea2a73a1db446301b0a9
Ryan Parman <ryan@ryanparman.com>
2023-09-04 18:47:28 -0600

    docs(terraform): Updated examples.

--- examples/data-sources/corefunc_env_ensure/data-source.tf
+++ examples/data-sources/corefunc_env_ensure/data-source.tf
@@ -20,6 +20,10 @@ data "corefunc_env_ensure" "aws_pager" {
   name = "AWS_PAGER"
 }
 
+output "aws_pager_value" {
+  value = data.corefunc_env_ensure.aws_pager.value
+}
+
 #=> [Error] Problem with Environment Variable: environment variable
 #=>         AWS_PAGER is not defined
 
@@ -29,6 +33,10 @@ data "corefunc_env_ensure" "aws_pager" {
 data "corefunc_env_ensure" "aws_vault" {
   name    = "AWS_VAULT"
   pattern = "(non)?prod$" # Must end with "prod" or "nonprod".
+}
+
+output "aws_vault_value" {
+  value = data.corefunc_env_ensure.aws_vault.value
 }
 
 #=> [Error] Problem with Environment Variable: environment variable
