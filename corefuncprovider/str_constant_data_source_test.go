3ff53ae9faa150eeffeb0096d14aa49591658c79
Ryan Parman <ryan@ryanparman.com>
2023-11-19 17:55:51 -0800

    build: Replaced much of Licensei with Trivy for licenses. (#66)
    * WIP
    
    * Replaced much of Licensei with Trivy for licenses.
    
    * lint: Ran consistency scan and fixed stuff.

--- corefuncprovider/str_constant_data_source_test.go
+++ corefuncprovider/str_constant_data_source_test.go
@@ -38,7 +38,7 @@ func TestAccStrConstantDataSource(t *testing.T) {
 			strings.TrimSpace(name),
 		)
 
-		buf := new(bytes.Buffer)
+		buf := &bytes.Buffer{}
 		tmpl := template.Must(
 			template.ParseFiles("str_constant_data_source_fixture.tftpl"),
 		)
