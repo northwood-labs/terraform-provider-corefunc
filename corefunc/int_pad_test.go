b0984dfc7d1cd1c520177d537c2dee099d238892
Ryan Parman <ryan@ryanparman.com>
2023-11-24 17:27:54 -0800

    feat: Begin implementations of StrLeftPad and IntLeftPad.

---
+++ corefunc/int_pad_test.go
@@ -0,0 +1,91 @@
+// Copyright 2023, Ryan Parman
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package corefunc
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/northwood-labs/terraform-provider-corefunc/testfixtures"
+)
+
+func ExampleIntLeftPad() {
+	output1 := IntLeftPad(255, 5)
+	fmt.Println(output1)
+
+	output2 := IntLeftPad(0b11111111, 5)
+	fmt.Println(output2)
+
+	output3 := IntLeftPad(0xFF, 5)
+	fmt.Println(output3)
+
+	// Output:
+	// 00255
+	// 00255
+	// 00255
+}
+
+func TestIntLeftPad(t *testing.T) {
+	for name, tc := range testfixtures.IntLeftPadTestTable {
+		t.Run(name, func(t *testing.T) {
+			output := IntLeftPad(tc.Input, tc.PadWidth)
+
+			if output != tc.Expected {
+				t.Errorf("Expected %s, got %s", tc.Expected, output)
+			}
+		})
+	}
+}
+
+func BenchmarkIntLeftPad(b *testing.B) {
+	b.ReportAllocs()
+
+	for name, tc := range testfixtures.IntLeftPadTestTable {
+		b.Run(name, func(b *testing.B) {
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				_ = IntLeftPad(tc.Input, tc.PadWidth) // lint:allow_unhandled
+			}
+		})
+	}
+}
+
+func BenchmarkIntLeftPadParallel(b *testing.B) {
+	b.ReportAllocs()
+
+	for name, tc := range testfixtures.IntLeftPadTestTable {
+		b.Run(name, func(b *testing.B) {
+			b.ResetTimer()
+			b.RunParallel(func(pb *testing.PB) {
+				for pb.Next() {
+					_ = IntLeftPad(tc.Input, tc.PadWidth) // lint:allow_unhandled
+				}
+			})
+		})
+	}
+}
+
+func FuzzIntLeftPad(f *testing.F) {
+	for _, tc := range testfixtures.IntLeftPadTestTable {
+		f.Add(tc.Input)
+		f.Add(int64(tc.PadWidth))
+	}
+
+	f.Fuzz(
+		func(t *testing.T, in int64) {
+			_ = IntLeftPad(in, int(in)) // lint:allow_unhandled
+		},
+	)
+}
---
+++ corefunc/str_pad.go
@@ -0,0 +1,51 @@
+// Copyright 2023, Ryan Parman
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package corefunc
+
+import (
+	"strings"
+)
+
+/*
+StrLeftPad pads a string on the left side with a given string until it reaches
+the desired width.
+
+----
+
+  - str (string): The string to pad.
+
+  - padWidth (int): The total width of the padded string. If `str` is equivalent
+    length or longer, then no padding will be applied.
+
+  - padStr (string): The string to use as padding. If unspecified, will use a
+    single space character.
+*/
+func StrLeftPad(str string, padWidth int, padStr ...rune) string {
+	if len(str) > padWidth {
+		return str
+	}
+
+	pStr := ' '
+
+	if padWidth < 0 {
+		padWidth = 0
+	}
+
+	if len(padStr) > 0 {
+		pStr = padStr[0]
+	}
+
+	return strings.Repeat(string(pStr), padWidth-len(str)) + str
+}
