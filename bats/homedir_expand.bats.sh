c4459ad7664dc6a1c93b3585705dc46d72cb76d2
Ryan Parman <ryan@ryanparman.com>
2024-02-02 17:14:44 -0700

    test: Updated BATS tests with Shellcheck feedback.

--- bats/homedir_expand.bats.sh
+++ bats/homedir_expand.bats.sh
@@ -4,8 +4,7 @@
 @test "corefunc_homedir_expand: attrs" {
     run bash -c "tfschema data show -format=json corefunc_homedir_expand | jq -Mrc '.attributes[]'"
 
-    [ "$status" -eq 0 ]
-    [[ ${lines[0]} == '{"name":"id","type":"number","required":false,"optional":false,"computed":true,"sensitive":false}' ]]
-    [[ ${lines[1]} == '{"name":"path","type":"string","required":true,"optional":false,"computed":false,"sensitive":false}' ]]
-    [[ ${lines[2]} == '{"name":"value","type":"string","required":false,"optional":false,"computed":true,"sensitive":false}' ]]
+    [[ "${status}" -eq 0 ]]
+    [[ ${lines[0]} == '{"name":"path","type":"string","required":true,"optional":false,"computed":false,"sensitive":false}' ]]
+    [[ ${lines[1]} == '{"name":"value","type":"string","required":false,"optional":false,"computed":true,"sensitive":false}' ]]
 }
