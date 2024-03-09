19ca7fdf48a279a018f3b43fad7fda898d00556f
Ryan Parman <ryan@ryanparman.com>
2024-03-09 15:45:38 -0700

    test: Make some adjustments for the test suite.

--- terratest/functions/env.tf
+++ terratest/functions/env.tf
@@ -1,9 +1,4 @@
-# output "env_ensure_fn" {
-#   description = "This returns the AWS_REGION environment variable."
-#   value       = provider::corefunc::env_ensure("AWS_REGION", "^([a-z]{2})-([^-]+)-(\\d)$")
-# }
-
 output "env_ensure_fn" {
-  description = "This returns the AWS_REGION environment variable."
-  value       = provider::corefunc::env_ensure("AWS_VAULT", "(non)?prod$")
+  description = "This returns the GOROOT environment variable."
+  value       = provider::corefunc::env_ensure("GOROOT")
 }
