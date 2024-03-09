d48f0ca4ea443bcdd333617893b7d483648ee7ef
Ryan Parman <ryan@ryanparman.com>
2024-03-09 15:45:38 -0700

    test: Make some adjustments for the test suite.

---
+++ bats/ds_str_iterative_replace.bats.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bats
+# https://bats-core.readthedocs.io/en/stable/writing-tests.html
+
+@test "corefunc_str_iterative_replace: attrs" {
+    run bash -c "tfschema data show -format=json corefunc_str_iterative_replace | jq -Mrc '.attributes[]'"
+
+    [[ ${status} -eq 0 ]]
+    [[ ${lines[0]} == '{"name":"replacements","type":"list(object({new=string,old=string}))","required":true,"optional":false,"computed":false,"sensitive":false}' ]]
+    [[ ${lines[1]} == '{"name":"string","type":"string","required":true,"optional":false,"computed":false,"sensitive":false}' ]]
+    [[ ${lines[2]} == '{"name":"value","type":"string","required":false,"optional":false,"computed":true,"sensitive":false}' ]]
+}
