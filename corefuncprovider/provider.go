05972caed0a634b3dffe5e16444453fc740ba487
Ryan Parman <ryan@ryanparman.com>
2024-03-10 20:57:17 -0600

    docs: Updated docs around OpenTofu and TF 1.8.

--- corefuncprovider/provider.go
+++ corefuncprovider/provider.go
@@ -62,27 +62,29 @@ func (p *coreFuncProvider) Schema(ctx context.Context, _ provider.SchemaRequest,
 
 	resp.Schema = schema.Schema{
 		MarkdownDescription: strings.TrimSpace(dedent.Dedent(`
-        Utilities that should have been Terraform _core functions_.
+        Utilities that should have been Terraform/OpenTofu _core functions_.
 
         While some of these _can_ be implemented in HCL, some of them begin to
         push up against the limits of Terraform and the HCL2 configuration
         language. We also perform testing using the
         [Terratest](https://terratest.gruntwork.io) framework on a regular basis.
-        Exposing these functions as both a Go library as well as a Terraform
-        provider enables us to use the same functionality in both our Terraform
+        Exposing these functions as both a Go library as well as a Terraform/OpenTofu
+        provider enables us to use the same functionality in both our Terraform/OpenTofu
         applies as well as while using a testing framework.
 
-        Since Terraform doesn't have the concept of user-defined functions, the
-        next step to open up the possibilities is to write a custom Terraform
-        Provider which has the functions built-in, using Terraform's existing
-        support for inputs and outputs.
+        Since earlier versions of Terraform/OpenTofu didn't have the concept of
+        user-defined functions, the next step to open up the possibilities was
+        to write a custom Provider which has the functions built-in, using
+        existing support for inputs and outputs.
 
-        **This does not add new syntax or constructs to Terraform.** Instead it
-        uses the _existing_ concepts around Providers, Resources, Data Sources,
-        Variables, and Outputs to expose new custom-built functionality.
+        **This does not add new syntax or constructs.** Instead it uses the
+        _existing_ concepts around Providers, Resources, Data Sources,
+        Variables, Outputs, and Functions (1.8) to expose new custom-built
+        functionality.
 
         The goal of this provider is not to call any APIs, but to provide
-        pre-built functions in the form of _Data Sources_.
+        pre-built functions in the form of _Data Sources_ or _Provider
+        Functions_ (1.8).
         `)),
 	}
 
