9f31a6d95ae71e0b8b475f57e55f82906733b6f3
Ryan Parman <ryan@ryanparman.com>
2024-03-11 11:32:05 -0600

    docs: Small fixes to the docs.

--- examples/data-sources/corefunc_str_iterative_replace/data-source.tf
+++ examples/data-sources/corefunc_str_iterative_replace/data-source.tf
@@ -1,34 +1,30 @@
 data "corefunc_str_iterative_replace" "replacements" {
   string = "This is a string for testing replacements. New Relic. Set-up."
-
-  replacements {
-    old = "."
-    new = ""
-  }
-
-  replacements {
-    old = " "
-    new = "_"
-  }
-
-  replacements {
-    old = "-"
-    new = "_"
-  }
-
-  replacements {
-    old = "New_Relic"
-    new = "datadog"
-  }
-
-  replacements {
-    old = "This"
-    new = "this"
-  }
-
-  replacements {
-    old = "Set_up"
-    new = "setup"
-  }
+  replacements = [
+    {
+      old = "."
+      new = ""
+    },
+    {
+      old = " "
+      new = "_"
+    },
+    {
+      old = "-"
+      new = "_"
+    },
+    {
+      old = "New Relic"
+      new = "datadog"
+    },
+    {
+      old = "This"
+      new = "this"
+    },
+    {
+      old = "Set-up"
+      new = "setup"
+    },
+  ]
 }
 #=> this_is_a_string_for_testing_replacements_datadog_setup
