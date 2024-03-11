ae832eae53efcf83b28811cf831da971db7f73ca
Ryan Parman <ryan@ryanparman.com>
2024-03-11 14:30:55 -0600

    docs: Polishing up the provider function documentation.

--- examples/functions/corefunc_url_parse/function.tf
+++ examples/functions/corefunc_url_parse/function.tf
@@ -1,5 +1,20 @@
-output "@TODO" {
-    value = provider::corefunc::url_parse("@TODO")
+output "url_parse_fn" {
+  description = "This returns a parsed URL."
+  value       = provider::corefunc::url_parse("HTTP://u:p@example.com:80/foo?q=1#bar")
 }
 
-#=> @TODO
+#=> normalized = "http://u:p@example.com/foo?q=1#bar"
+#=> normalized_nofrag = "http://u:p@example.com/foo?q=1"
+#=> protocol = "http:"
+#=> scheme = "http"
+#=> username = "u"
+#=> password = "p"
+#=> host = "example.com"
+#=> hostname = "example.com"
+#=> port = ""
+#=> decoded_port = 80
+#=> path = "/foo"
+#=> search = "?q=1"
+#=> query = "q=1"
+#=> hash = "#bar"
+#=> fragment = "bar"
