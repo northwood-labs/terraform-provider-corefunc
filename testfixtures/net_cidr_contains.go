9069983ceeb29d025f41c8b03dd12d97f0c84141
Ryan Parman <ryan@ryanparman.com>
2024-11-10 16:45:52 -0700

    fix: Added some missing license headers.

--- testfixtures/net_cidr_contains.go
+++ testfixtures/net_cidr_contains.go
@@ -1,7 +1,7 @@
 // Copyright 2023-2024, Northwood Labs
 // Copyright 2023-2024, Ryan Parman <rparman@northwood-labs.com>
 //
-// Licensed under the Apache License, Version 2.0 (the "License";
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
