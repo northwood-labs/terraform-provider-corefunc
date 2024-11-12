5ea47e87488bd196b0a36ac2c960241d1ff3d2e1
Ryan Parman <ryan@ryanparman.com>
2024-11-12 12:21:06 -0700

    feat: Add support for counting bytes in strings.

---
+++ corefunc/str_byte_length.go
@@ -0,0 +1,31 @@
+// Copyright 2023-2024, Northwood Labs
+// Copyright 2023-2024, Ryan Parman <rparman@northwood-labs.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package corefunc
+
+import "github.com/mattn/go-runewidth"
+
+/*
+StrByteLength returns the number of bytes/runes in a string. This is different
+from the number of characters, which is what Terraform and OpenTofu's `length()`
+function returns for strings.
+
+----
+
+  - str (string): The string with which to count bytes/runes.
+*/
+func StrByteLength(str string) int {
+	return runewidth.StringWidth(str)
+}
---
+++ corefunc/str_byte_length_test.go
@@ -0,0 +1,55 @@
+// Copyright 2023-2024, Northwood Labs
+// Copyright 2023-2024, Ryan Parman <rparman@northwood-labs.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package corefunc
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/northwood-labs/terraform-provider-corefunc/testfixtures"
+)
+
+func ExampleStrByteLength() {
+	output := StrByteLength("abcde")
+	fmt.Println(output)
+
+	output = StrByteLength("\x00")
+	fmt.Println(output)
+
+	output = StrByteLength("👁")
+	fmt.Println(output)
+
+	output = StrByteLength("■㈱の世界①")
+	fmt.Println(output)
+
+	// Output:
+	// 5
+	// 0
+	// 1
+	// 10
+}
+
+func TestStrByteLength(t *testing.T) { // lint:allow_complexity
+	for name, tc := range testfixtures.StrByteLengthTestTable {
+		t.Run(name, func(t *testing.T) {
+			output := StrByteLength(tc.Input)
+
+			if output != tc.Expected {
+				t.Errorf("Expected %d, got %d", tc.Expected, output)
+			}
+		})
+	}
+}
