3ff01501d05c9e5380f6c6b62c607a9ef03155b3
Ryan Parman <ryan@ryanparman.com>
2025-08-13 19:03:16 -0600

    fix!: Remove the already-deprecated truncate_label functionality.

--- terratest/functions/str_truncate.tf
+++
@@ -1,8 +0,0 @@
-output "str_truncate_fn" {
-  description = "This is a truncation output."
-  value = provider::corefunc::str_truncate_label(
-    64,
-    "NW-ZZZ-CLOUD-TEST-APP-CLOUD-PROD-CRIT",
-    "K8S Pods Not Running Deployment Check",
-  )
-}
