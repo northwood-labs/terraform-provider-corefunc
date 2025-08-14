0c00179785470f8a09b9060b9643c4be768bdd11
Ryan Parman <ryan@ryanparman.com>
2025-08-13 19:03:16 -0600

    fix!: Remove the already-deprecated truncate_label functionality.

--- examples/functions/corefunc_str_truncate_label/function.tf
+++
@@ -1,9 +0,0 @@
-output "label" {
-  value = provider::corefunc::str_truncate_label(
-    64,
-    "NW-ZZZ-CLOUD-TEST-APP-CLOUD-PROD-CRIT",
-    "K8S Pods Not Running Deployment Check",
-  )
-}
-
-#=> NW-ZZZ-CLOUD-TEST-APP-CLOUD-PR…: K8S Pods Not Running Deploymen…
