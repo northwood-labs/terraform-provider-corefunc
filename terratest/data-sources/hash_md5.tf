e05adc45d673132f38cb61f9b43881e689565728
Ryan Parman <ryan@ryanparman.com>
2025-11-30 13:17:11 -0700

    test: Terratest for MD5 hashing with Base64 output.

--- terratest/data-sources/hash_md5.tf
+++ terratest/data-sources/hash_md5.tf
@@ -6,3 +6,12 @@ output "hash_md5_ds" {
   description = "The MD5 hash of the input string."
   value       = data.corefunc_hash_md5.md5.value
 }
+
+data "corefunc_hash_md5_base64" "md5_base64" {
+  input = "hello world"
+}
+
+output "hash_md5_base64_ds" {
+  description = "The MD5 hash of the input string."
+  value       = data.corefunc_hash_md5_base64.md5_base64.value
+}
